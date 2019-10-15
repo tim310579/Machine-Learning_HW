b3f8b736a62469e2dd47b6ca5e17927f17e009b8
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-10-15 16:14:06 +0800

    Add files via upload

--- mushroom.py
+++ mushroom.py
@@ -12,44 +12,13 @@ df.columns = [c]
 for col in df.columns:
     delete = df[df[col] == "?"].index
     df.drop(delete, inplace = True)
-#print(df.tail())
-#ct = df.groupby(["eaten"]).size()
-val_fre = pd.DataFrame()
-#mat = np.zeros((23, 1), dtype = np.int)    #probility matrix
-arr_e = []
-arr_p = []
-for i in range (0,24):
-    arr_e.append([])
-    arr_p.append([])
-    arr_e[i].append(0)
-    arr_p[i].append(0)
-#print(arr[14][0])
-cha = []
-cha2 = []
-type_num_e = []   #means number of types in every feature
-type_num_p = []
-type_num_e.append(0)
-type_num_p.append(0)
 df = df.sample(frac=1).reset_index(drop = True)   #shuffle
-with open('output.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-
-    df.to_csv('output.data')
-
 count = len(df)
 tmp = count*0.7
 tmp2 = int(tmp)
 df_test = df[tmp2:count]
 df = df[0:tmp2]
 
-#print(len(df))
-'''with open('output2.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    df.to_csv('output2.data')
-with open('outputtest.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    df_test.to_csv('outputtest.data')
-'''
 df_e = pd.DataFrame()
 df_e = df.copy()
 df_p = df.copy()
@@ -68,130 +37,26 @@ ppp = pd.DataFrame()
 epep = []
 for col in df.columns:
     ppp = ppp.append(df[col].value_counts(normalize = True))
-    #epep.append(ppp.ix[[0]].values[0][0])
-    #epep.append(ppp.values[1][0])
-    #epep = [ppp.iat[0, 0], ppp.iat[0,1]]
     break
 epep.append((ppp.iat[0,0]))
 epep.append((ppp.iat[0,1]))
-print(ppp, "ppppppppppppppppppppppppppppppppppppp")
-print(epep)
-#print(len(df), len(df_e), len(df_p))
-i = 0
 tmp_test0 = pd.DataFrame()
 for col in df_e.columns:
-    i = i + 1
-    val_fre = pd.DataFrame()
-    #val_fre = val_fre.append(df[col].value_counts())
-    val_fre = val_fre.append(df_e[col].value_counts(normalize = True))
-    tmp_test0 = tmp_test0.append(df_e[col].value_counts(normalize = True))
-    #print(val_fre)
-    #cha.append([])
-    cha.append(val_fre.columns.values.tolist()) 
-    #print(val_fre.columns.values.tolist())
-    j = 0
-    #print(val_fre)
-    jk = val_fre.shape[1]
-    type_num_e.append(jk)
-    for j in range(0, jk):
-            #print(val_fre.values)
-         arr_e[i].append(val_fre.ix[[0]].values[0][j])    #probility
-    #val_fre.drop(val_fre.index[:1])
-i = 0
+    tmp_test0 = tmp_test0.append(df_e[col].value_counts(normalize = True))  #probability in cond of etible
 tmp_test = pd.DataFrame()
 for col in df_p.columns:
-    i = i + 1
-    val_fre = pd.DataFrame()
-    #val_fre = val_fre.append(df[col].value_counts())
-    val_fre = val_fre.append(df_p[col].value_counts(normalize = True))
-    tmp_test = tmp_test.append(df_p[col].value_counts(normalize = True))
-    cha2.append(val_fre.columns.values.tolist()) 
-    #print(val_fre.columns.values.tolist())
-    j = 0
-    #print(val_fre)
-    jk = val_fre.shape[1]
-    type_num_p.append(jk)
-    for j in range(0, jk):
-            #print(val_fre.values)
-         arr_p[i].append(val_fre.ix[[0]].values[0][j])    #probility in condi p
-#print(type_num_p)
+    tmp_test = tmp_test.append(df_p[col].value_counts(normalize = True))    #probibility in condition of poison    
 tmp_test.index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
 tmp_test0.index = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
-print(tmp_test.head())
+tmp_test = tmp_test.fillna(0)  #poison
+tmp_test0 = tmp_test0.fillna(0) #etible
+cnt = 0
+cnt2 = 0
 i = 0
-cha_test = []
-for col in df_test.columns:
-    i = i + 1
-    val_fre = pd.DataFrame()
-    val_fre = val_fre.append(df_test[col].value_counts(normalize = True))
-    cha_test.append(val_fre.columns.values.tolist())
-print(cha_test) 
-ct = 1
-'''
-print(arr_e)
-print(arr_p)
-print(cha)
-print(cha2)
-print(type_num_e)
-print(type_num_p)'''
-type_num_test = [0, 2, 6, 4, 8, 2, 7, 2, 2, 2, 9, 2, 4, 4, 4, 7, 7, 1, 2, 3, 4, 6, 6, 6]
-#type_num_test= [0, 2, 5, 4, 8, 2, 5, 2, 2, 2, 9, 2, 4, 4, 4, 6, 6, 1, 2, 3, 4, 5, 5, 6]
-'''ct = 1
-for cl in df_e.columns:
-    for nums in range(1, type_num_test[ct]+1):
-        #print(nums)
-        df_e[cl].replace(cha_test[ct-1][nums-1], nums, inplace = True)     #transform
-    ct = ct +1  
-ct = 1
-for cl in df_p.columns:
-    for nums in range(1, type_num_test[ct]+1):
-        #print(nums)
-        df_p[cl].replace(cha_test[ct-1][nums-1], nums, inplace = True) #transform
-    ct = ct +1                                          #to int struct
-
-type_num_test = []
-#cha_test = []
-for j in range(0, 24):
-    if type_num_e[j] > type_num_p[j]:
-        type_num_test.append(type_num_e[j])
-    else:
-        type_num_test.append(type_num_p[j])
-type_num_test[1] = 2
-print(type_num_test)
-print(cha)
-print(cha2)
-print(cha_test)
-
-ct = 1
-for cl in df_test.columns:
-    for nums in range(1, type_num_test[ct]+1):
-        #print(nums)
-        df_test[cl].replace(cha_test[ct-1][nums-1], nums, inplace = True) #transform
-    ct = ct +1 
-print(df_e.tail())
-print(df_p.tail())
-print(df_test.tail())
-
-with open('outputtest.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-
-    df_test.to_csv('outputtest.data')
-with open('output.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-
-    df_e.to_csv('output.data')
-with open('output2.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-
-    df_p.to_csv('output2.data')
-
-ft = 1
-print(arr_e)
-'''
-ft = 1
-outcome = []
-#print(arr_e(2,1))
-print(tmp_test0)
+TP = 0
+TN = 0
+FP = 0
+FN = 0
 for y in range (0, count-tmp2):
     ft1 = 1
     ft2 = 1
@@ -201,27 +66,30 @@ for y in range (0, count-tmp2):
         ft2 = ft2*tmp_test0.at[ik, df_test.iat[y, ik]]
     #$if(df_test.iat[y,0] == 'e'):
     ft2 = ft2/epep[0]   #eat
-    #else:
+    
     ft1 = ft1/epep[1]    #poisin
-    #print(ft1, ft2)
-    if(np.isnan(ft1) == True):
-        ft1 = 0
-    if(np.isnan(ft2) == True):
-        ft2 = 0
+      
     if(ft1 <= ft2):
-        outcome.append('e')
+        predict = 'e'
+        if(predict == df_test.iat[i, 0]):
+            TP = TP + 1
+        else:
+            FP = FP + 1
     else:
-        outcome.append('p')
-#print(df_test.iat[0, 0])
- #   break
-print(outcome)    
-#print(arr)
-#print(ft)
-'''
-for k in range(1, 22):
-    for l in range(0,3):
-        print(arr[k][l])
-with open('output.data', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    df.to_csv('output.data')'''
-
+        predict = 'p'
+        if(predict == df_test.iat[i, 0]):
+            TN = TN + 1
+        else:
+            FN = FN + 1
+    i = i + 1
+print(TP, FN, FP, TN)
+outcome = pd.DataFrame(index = ['Actual Positive(etible)', 'Actual Negative(poison)'], columns = ['Predict Positive(etible)', 'Predict negative(poison)'])
+outcome['Predict Positive(etible)'] = [TP, FP]
+outcome['Predict negative(poison)'] = [FN, TN]
+print(outcome)
+acc = (TP+TN) / (TP+TN+FP+FN)
+rec = TP/(TP+FN)
+pre = TP/(TP+FP)
+print("Accuracy:", acc)
+print("Sensitivity(Recall):", rec)
+print("Precision:", pre)
