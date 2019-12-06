f16d58d1f76d37b17899cb4c9447f59edf921011
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-12-06 15:16:31 +0800

    first part OK

--- ML_hw3/regress.py
+++ ML_hw3/regress.py
@@ -60,21 +60,23 @@ def X2():
     X = df['x']
     Y = df['y']
     plt.plot(X,Y, 'o')
-    x = np.arange(-5, 5)
+    x = np.arange(-5, 6)
     y = (tmp0+tmp1*x)
-    print('Fitting line:', tmp1, 'X^1 + ', tmp0)
+    print('n = 2')
+    print('Fitting line:', tmp1, 'X^1 +', tmp0)
     err = 0;
     for i in range(0, siz):
         err += pow(((tmp0+tmp1*X[i])-Y[i]), 2)
-    print(err)
+    print('Total error : ', err)
+    print('')
     plt.plot(x,y)
 
     plt.xlabel("x-axis")
     plt.ylabel("y-axis")
     plt.title("For X2")
-    #plt.show()
-def compute_det2(ATA):
-    ret = ATA[0][0]*ATA[1][1] - ATA[1][0]*ATA[0][1]
+    plt.show()
+def det2(a, b, c, d):
+    ret = a*b - c*d
     return ret
 def compute_det3(ATA):
     pos = 0
@@ -106,18 +108,56 @@ def X3():
     ATA = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]], dtype = np.float)
     #ATA = np.array([[0,0],[0,0]], dtype = np.float)
     #for i in range(0, siz):
-    print(ATA.shape)
+    #print(ATA.shape)
     for j in range(3):
         for k in range(3):
             for i in range(siz):
                 ATA[j][k] += AT[j][i] * A[i][k]
 
-    print(ATA)
+    #print(ATA)
     #print(compute_inv2(ATA).dot(ATA))
-    det = compute_det2(ATA)
-    print(det)
-    #print(np.linalg.det(ATA))
+    det = compute_det3(ATA)
     
+    ATAI = np.zeros([3, 3])
+    ATAI[0][0] = det2(ATA[1][1], ATA[2][2], ATA[1][2], ATA[2][1])
+    ATAI[0][1] = -det2(ATA[0][1], ATA[2][2], ATA[0][2], ATA[2][1])
+    ATAI[0][2] = det2(ATA[0][1], ATA[1][2], ATA[0][2], ATA[1][1])
+    ATAI[1][0] = -det2(ATA[0][1], ATA[2][2], ATA[1][2], ATA[2][0])
+    ATAI[1][1] = det2(ATA[0][0], ATA[2][2], ATA[0][2], ATA[2][0])
+    ATAI[1][2] = -det2(ATA[0][0], ATA[1][2], ATA[0][2], ATA[1][0])
+    ATAI[2][0] = det2(ATA[1][0], ATA[2][1], ATA[1][1], ATA[2][0])
+    ATAI[2][1] = -det2(ATA[0][0], ATA[2][1], ATA[0][1], ATA[2][0])
+    ATAI[2][2] = det2(ATA[0][0], ATA[1][1], ATA[0][1], ATA[1][0])
+    ATAI /= det
 
+    ATAIAT = np.zeros([3, siz])
+    for i in range(siz):
+        ATAIAT[0][i] = ATAI[0][0]*AT[0][i] + ATAI[0][1]*AT[1][i] + ATAI[0][2]*AT[2][i]
+        ATAIAT[1][i] = ATAI[1][0]*AT[0][i] + ATAI[1][1]*AT[1][i] + ATAI[1][2]*AT[2][i]
+        ATAIAT[2][i] = ATAI[2][0]*AT[0][i] + ATAI[2][1]*AT[1][i] + ATAI[2][2]*AT[2][i]
+    tmp = [0, 0, 0]
+    #print(B)
+    for i in range(siz):
+        tmp[0] += ATAIAT[0][i]*B[i][0]
+        tmp[1] += ATAIAT[1][i]*B[i][0]
+        tmp[2] += ATAIAT[2][i]*B[i][0]
+    #print(tmp)
+    X = np.array(df['x'])
+    Y = np.array(df['y'])
+    plt.plot(X, Y, 'o')
+    #print(dfx[siz-1])
+    x = np.arange(-5, 6)
+    y = (tmp[0] + tmp[1]*x + tmp[2]*x*x)
+    err = float(0)
+    print('n = 3')
+    print('Fitting line:', tmp[2],'X^2 +', tmp[1], 'X^1 +', tmp[0])
+    for i in range(0, siz):
+        err += pow(((tmp[0]+tmp[1]*X[i]+tmp[2]*X[i]*X[i]) - Y[i]), 2)
+    print('Total error : ', err)
+    plt.plot(x, y)
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.title("For X3")
+    plt.show()
 X2()
 X3()
