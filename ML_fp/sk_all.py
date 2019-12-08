05b2732e374e65aec03c7dc8455e820ee82f3274
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-12-08 21:56:43 +0800

    with plot

--- ML_fp/sk_all.py
+++ ML_fp/sk_all.py
@@ -1,3 +1,4 @@
+import sklearn
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
@@ -6,54 +7,60 @@ from sklearn import preprocessing, linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model  import LogisticRegression
 from sklearn.neural_network import MLPClassifier
-
+from sklearn import model_selection
+from sklearn.model_selection import KFold
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import LeavePOut
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import plot_precision_recall_curve
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 import numpy as np
+from sklearn import metrics
+from sklearn.preprocessing import label_binarize
 
 
+def draw(name, model, y_bin, test, cl):
+    y_score = model.predict_proba(test)
+    fpr, tpr, thresholds = metrics.roc_curve(y_bin.ravel(),y_score.ravel())
+    auc = metrics.auc(fpr, tpr)
+    mpl.rcParams['font.sans-serif'] = u'SimHei'
+    mpl.rcParams['axes.unicode_minus'] = False
+    plt.plot(fpr, tpr, c = cl, lw = 2, alpha = 0.7, label = u'AUC=%.3f' % auc)
+    plt.plot((0, 1), (0, 1), c = '#808080', lw = 1, ls = '--', alpha = 0.7)
+    plt.xlim((-0.01, 1.02))
+    plt.ylim((-0.01, 1.02))
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.xlabel('False Positive Rate', fontsize=13)
+    plt.ylabel('True Positive Rate', fontsize=13)
+    plt.grid(b=True, ls=':')
+    plt.legend(loc='lower right', fancybox=True, framealpha=0.8, fontsize=12)
+    plt.title(u'ROC AUC for 5 models', fontsize=17)
 
-def print_matrix(name, result, test_target):
-    
-    mat = np.zeros([3, 3])
-    for i in range(len(result)):
-        if(result[i] == 1):
-            if(test_target[i] == 1):
-                mat[0][0] += 1
-            elif(test_target[i] == 2):
-                mat[1][0] += 1
-            else:
-                mat[2][0] += 1
-        elif(result[i] == 2):
-            if(test_target[i] == 1):
-                mat[0][1] += 1
-            elif(test_target[i] == 2):
-                mat[1][1] += 1
-            else:
-                mat[2][1] += 1
-        else:
-            if(test_target[i] == 1):
-                mat[0][2] += 1
-            elif(test_target[i] == 2):
-                mat[1][2] += 1
-            else:
-                mat[2][2] += 1
-
+def print_matrix(name, model, test, test_target):
+    predict = model.predict(test)
+    mat = confusion_matrix(test_target, predict)
+    acc = model.score(test, test_target)
+    pre = precision_score(test_target, predict, average=None)
+    rec = recall_score(test_target, predict, average=None)
     data_frame = pd.DataFrame(mat, index = ['Actual train, bus, ship', 'Actual HSR, airplane', 'Actual drive, ride'], columns = ['Predict train, bus, ship', 'HSR, airplane', 'drive, ride'])
     print(name,'----------------------------------------------------------------------')
     print(data_frame)
-    acc = (mat[0][0] + mat[1][1] + mat[2][2])/len(result)
-    rec = []
-    pre = []
-    for i in range(3):
-        rec.append(mat[i][i]/(mat[i][0] + mat[i][1] + mat[i][2]))
-        pre.append(mat[i][i]/(mat[0][i] + mat[1][i] + mat[2][i]))
-    rec_pre = np.array([rec, pre])
-
-    rp = pd.DataFrame(rec_pre, index = ['Sensitivity(Recall)', 'Precision'], columns = ['train, bus, ship', 'HSR, airplane', 'drive, ride'])
+    print('')
     print('Accuracy: ', acc)
-    print(rp)
+    print('Precision: ', pre)
+    print('Recall: ', rec)
     print(' ')
     print(' ')
+
 
 
 fea = ['Choose hometown or travel', 'job', 'back home frequency', 'location of work (school)', 'hometown location', 'distance between the above two', 'interpersonal relationship', 'family relationship', 'gender', 'financial situation(income)', 'have boy/girlfriend/husband/wife']
@@ -72,62 +79,43 @@ le = preprocessing.LabelEncoder()
 for col in fea:   #transform to numeric data
     encode_col = le.fit_transform(train[col])
     train[col] = encode_col
-train['transportation'].replace(['train bus or ship'], [1], inplace = True)
-train['transportation'].replace(['HSR or airplane'], [2], inplace = True)
-train['transportation'].replace(['drive or ride by yourself'], [3], inplace = True)
+train['transportation'].replace(['train bus or ship'], [0], inplace = True)
+train['transportation'].replace(['HSR or airplane'], [1], inplace = True)
+train['transportation'].replace(['drive or ride by yourself'], [2], inplace = True)
 
+train_all = train.copy()
+train_all = train_all.drop(columns = 'transportation')
+test_all = train['transportation']
+#test_all = label_binarize(test_all, classes=[0, 1, 2])
+#train, test, target, test_target = model_selection.train_test_split(train_all, test_all, test_size=0.30, random_state=100)
+#result_k0 = model_selection.cross_val_score(model0, train_all, test_all, cv = kfold)
+#result_k1 = model_selection.cross_val_score(model1, train_all, test_all, cv = kfold)
+#result_k2 = model_selection.cross_val_score(model2, train_all, test_all, cv = kfold)
+#result_k3 = model_selection.cross_val_score(model3, train_all, test_all, cv = kfold)
+#result_k4 = model_selection.cross_val_score(model4, train_all, test_all, cv = kfold)
+#test_all = label_binarize(test_all,classes=[0,1,2])
+'''n_classes = test_all.shape[1]
 
-choose1 = train[train['transportation'] == 1].index
-choose2 = train[train['transportation'] == 2].index
-choose3 = train[train['transportation'] == 3].index
-
-df1 = train.loc[choose1]
-df2 = train.loc[choose2]
-df3 = train.loc[choose3]
-df1 = df1.reset_index(drop = True)
-df2 = df2.reset_index(drop = True)
-df3 = df3.reset_index(drop = True)
-len1 = len(df1)
-len2 = len(df2)
-len3 = len(df3)
-
-df1_test = df1[int(len1*0.7): len1]
-df2_test = df2[int(len2*0.7): len2]
-df3_test = df3[int(len3*0.7): len3]
-df1 = df1[0: int(len1*0.7)]
-df2 = df2[0: int(len2*0.7)]
-df3 = df3[0: int(len3*0.7)]
-
-train = pd.concat([df1, df2, df3], axis = 0)
-train = train.reset_index(drop = True)
-test = pd.concat([df1_test, df2_test, df3_test], axis =0)
-test = test.reset_index(drop = True)
-
-target = train['transportation']
-train = train.drop(columns = 'transportation')
-test_target = test['transportation']
-test = test.drop(columns = 'transportation')
-
-model0 = DecisionTreeClassifier(max_depth = 8).fit(train, target)
-model1 = RandomForestClassifier(max_depth = 8).fit(train, target)
-model2 = GaussianNB().fit(train, target)
-model3 = LogisticRegression().fit(train, target)
-model4 = MLPClassifier().fit(train, target)
-
+random_state = np.random.RandomState(0)
+n_samples, n_features = train_all.shape
+train_all = np.c_[train_all,random_state.randn(n_samples,200 * n_features)]
+'''
+train, test, target, test_target = train_test_split(train_all, test_all, test_size=0.30, random_state=0)
+n_class = 3
+y_bin = label_binarize(test_target, np.arange(n_class))
+alpha = np.logspace(-2, 2, 20)
+ 
+model = []
+model.append(DecisionTreeClassifier(max_depth = 8).fit(train, target))
+model.append(RandomForestClassifier(n_estimators=10, max_depth = 8, random_state=42).fit(train, target))
+model.append(GaussianNB().fit(train, target))
+model.append(LogisticRegression().fit(train, target))
+model.append(MLPClassifier().fit(train, target))
 result = []
-result.append(model0.predict(test))
-result.append(model1.predict(test))
-result.append(model2.predict(test))
-result.append(model3.predict(test))
-result.append(model4.predict(test))
-
-combine = []
-df_combine = pd.DataFrame(result)
-for col in df_combine.columns:
-    combine.append(df_combine[col].mode()[0])
-
 name = ['Decision Tree', 'Random Forest', 'Naive Bayes', 'Logistic Regression', 'Neural Network']
-
+color = ['r', 'g', 'b', 'y', 'k']
 for i in range(5):
-    print_matrix(name[i], result[i], test_target)
-print_matrix('Combine All', combine, test_target)
+    print_matrix(name[i], model[i], test, test_target)
+    draw(name[i], model[i], y_bin, test, color[i])
+#print(roc_auc_score(test_target, y_pred_proba))
+plt.show()
