9f84dd1a943abb8439a9726fe8e25e437e2f375c
tim310579 <43834434+tim310579@users.noreply.github.com>
2019-12-10 20:30:12 +0800

    add K fold

--- ML_fp/sk_all.py
+++ ML_fp/sk_all.py
@@ -22,6 +22,7 @@ import pandas as pd
 import numpy as np
 from sklearn import metrics
 from sklearn.preprocessing import label_binarize
+from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
 
 
 def draw(name, model, y_bin, test, cl):
@@ -65,23 +66,16 @@ def print_matrix(name, model, test, test_target):
 fea = ['Choose hometown or travel', 'job', 'back home frequency', 'location of work (school)', 'hometown location', 'distance between the above two', 'interpersonal relationship', 'family relationship', 'gender', 'financial situation(income)', 'have boy/girlfriend/husband/wife']
 
 train = pd.read_csv('tran.csv')
-train = train.sample(frac=1).reset_index(drop = True) #shuffle
+#train = train.sample(frac=1).reset_index(drop = True) #shuffle
 train.replace(np.nan, 23, inplace=True)
 for col in fea:
     train[col].replace(["?"],  [train[col].mode()], inplace = True)   #deal with missing
 delete =  train[train['Choose hometown or travel'] == 'travel'].index
 #train.drop(delete, inplace = True)
+#train = train.reset_index(drop = True)
 #train = train.drop('Choose hometown or travel', axis = 1)
-fea = ['Choose hometown or travel','age', 'job', 'back home frequency', 'location of work (school)', 'hometown location', 'distance between the above two', 'interpersonal relationship', 'family relationship', 'gender', 'financial situation(income)', 'have boy/girlfriend/husband/wife']
-
-le = preprocessing.LabelEncoder()
-for col in fea:   #transform to numeric data
-    encode_col = le.fit_transform(train[col])
-    train[col] = encode_col
-train['transportation'].replace(['train bus or ship'], [0], inplace = True)
-train['transportation'].replace(['HSR or airplane'], [1], inplace = True)
-train['transportation'].replace(['drive or ride by yourself'], [2], inplace = True)
-
+fea = ['Choose hometown or travel', 'job', 'back home frequency', 'location of work (school)', 'hometown location', 'distance between the above two', 'interpersonal relationship', 'family relationship', 'gender', 'financial situation(income)', 'have boy/girlfriend/husband/wife']
+'''
 for i in range(len(train)):
     if(train.iat[i, 1] < 18): train.iat[i, 1] = 0
     elif(train.iat[i, 1] >= 18 and train.iat[i, 1] < 22): train.iat[i, 1] = 1
@@ -90,24 +84,19 @@ for i in range(len(train)):
     elif(train.iat[i, 1] >= 30 and train.iat[i, 1] < 35): train.iat[i, 1] = 4
     elif(train.iat[i, 1] >= 35 and train.iat[i, 1] < 40): train.iat[i, 1] = 5
     else: train.iat[i, 1] = 6
-    
+'''
+le = preprocessing.LabelEncoder()
+for col in fea:   #transform to numeric data
+    encode_col = le.fit_transform(train[col])
+    train[col] = encode_col
+train['transportation'].replace(['train bus or ship'], [0], inplace = True)
+train['transportation'].replace(['HSR or airplane'], [1], inplace = True)
+train['transportation'].replace(['drive or ride by yourself'], [2], inplace = True)
+
 train_all = train.copy()
 train_all = train_all.drop(columns = 'transportation')
 test_all = train['transportation']
-#test_all = label_binarize(test_all, classes=[0, 1, 2])
-#train, test, target, test_target = model_selection.train_test_split(train_all, test_all, test_size=0.30, random_state=100)
-#result_k0 = model_selection.cross_val_score(model0, train_all, test_all, cv = kfold)
-#result_k1 = model_selection.cross_val_score(model1, train_all, test_all, cv = kfold)
-#result_k2 = model_selection.cross_val_score(model2, train_all, test_all, cv = kfold)
-#result_k3 = model_selection.cross_val_score(model3, train_all, test_all, cv = kfold)
-#result_k4 = model_selection.cross_val_score(model4, train_all, test_all, cv = kfold)
-#test_all = label_binarize(test_all,classes=[0,1,2])
-'''n_classes = test_all.shape[1]
 
-random_state = np.random.RandomState(0)
-n_samples, n_features = train_all.shape
-train_all = np.c_[train_all,random_state.randn(n_samples,200 * n_features)]
-'''
 train, test, target, test_target = train_test_split(train_all, test_all, test_size=0.30, random_state=0)
 n_class = 3
 y_bin = label_binarize(test_target, np.arange(n_class))
@@ -127,4 +116,12 @@ for i in range(6):
     print_matrix(name[i], model[i], test, test_target)
     draw(name[i], model[i], y_bin, test, color[i])
 #print(roc_auc_score(test_target, y_pred_proba))
+
+results = []
+kfold = KFold(n_splits=10, random_state = 42, shuffle = True)
+print('K-fold for K = 10')
+for i in range(6):
+    results.append(model_selection.cross_val_score(model[i], train_all, test_all, cv = kfold))
+    print(name[i], 'Accuracy : ',results[i].mean())
+
 plt.show()
